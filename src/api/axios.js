06fa86bb62a22d1b213b0a9045bc77b359bbc88c
fanta-fineapple <das1905@naver.com>
2022-07-04 16:17:19 +0900

    g

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
 
 const api = axios.create({
-  baseURL: `${PROXY}/ttb/api/`
+  baseURL: `/proxy/ttb/api/`
 });
 
 export const bookApi = {
